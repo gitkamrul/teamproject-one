211ed51ff18a259601cd4e6f3085b6b7d730d8cb
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-08 22:52:03 +0600

    change eshop file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-(function($){
-	$(document).ready(function() {	
+(function ($) {
+	$(document).ready(function () {
 
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery('.scrolltotop').click(function () {
+			jQuery('html').animate({
+				'scrollTop': '0px'
+			}, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
+			if (upto > 500) {
 				jQuery('.scrolltotop').fadeIn();
 			} else {
 				jQuery('.scrolltotop').fadeOut();
@@ -19,23 +21,23 @@
 
 		// code for menu
 
-		$(".p2header-left span").click(function (){
+		$(".p2header-left span").click(function () {
 			$(".menu").animate({
-				"left":"0%"
+				"left": "0%"
 			});
 			$("body").addClass("overflow")
 		});
 
-		$(".menu-left-heading span").click(function (){
+		$(".menu-left-heading span").click(function () {
 			$(".menu").animate({
-				"left":"-100%"
+				"left": "-100%"
 			});
 			$("body").removeClass("overflow")
 		})
 
 
 
-		
+
 	});
 })(jQuery);
 
@@ -45,13 +47,12 @@ const hr = document.getElementById("hr");
 const mn = document.getElementById("mn");
 const sc = document.getElementById("sc");
 
-
-setInterval(()=>{
+setInterval(() => {
 	let day = new Date();
-let hh = day.getHours()*30;
-let mm = day.getMinutes()*deg;
-let ss = day.getSeconds()*deg;
-hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-mn.style.transform = `rotateZ(${mm}deg)`;
-sc.style.transform = `rotateZ(${ss}deg)`;
-})
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * deg;
+	let ss = day.getSeconds() * deg;
+	hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+	mn.style.transform = `rotateZ(${mm}deg)`;
+	sc.style.transform = `rotateZ(${ss}deg)`;
+}, 1000);
